c9cfafe6d987fd0aae1fe11ff5d7f4c6a9c21b9b
F S <fs11@hotmail.fr>
2020-04-25 07:20:21 +0200

    aspnetuser

---
+++ WS01/Controllers/AspNetUsersController.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WS01.Models;
+
+namespace WS01.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class AspNetUsersController : Controller
+    {
+        private readonly WS01DBContext _context;
+
+        public AspNetUsersController(WS01DBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: AspNetUsers
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.AspNetUsers.ToListAsync());
+        }
+
+        // GET: AspNetUsers/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var aspNetUsers = await _context.AspNetUsers
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (aspNetUsers == null)
+            {
+                return NotFound();
+            }
+
+            return View(aspNetUsers);
+        }
+
+        // GET: AspNetUsers/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AspNetUsers/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,UserName,NormalizedUserName,Email,NormalizedEmail,EmailConfirmed,PasswordHash,SecurityStamp,ConcurrencyStamp,PhoneNumber,PhoneNumberConfirmed,TwoFactorEnabled,LockoutEnd,LockoutEnabled,AccessFailedCount")] AspNetUsers aspNetUsers)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(aspNetUsers);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(aspNetUsers);
+        }
+
+        // GET: AspNetUsers/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var aspNetUsers = await _context.AspNetUsers.FindAsync(id);
+            if (aspNetUsers == null)
+            {
+                return NotFound();
+            }
+            return View(aspNetUsers);
+        }
+
+        // POST: AspNetUsers/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("Id,UserName,NormalizedUserName,Email,NormalizedEmail,EmailConfirmed,PasswordHash,SecurityStamp,ConcurrencyStamp,PhoneNumber,PhoneNumberConfirmed,TwoFactorEnabled,LockoutEnd,LockoutEnabled,AccessFailedCount")] AspNetUsers aspNetUsers)
+        {
+            if (id != aspNetUsers.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(aspNetUsers);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AspNetUsersExists(aspNetUsers.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(aspNetUsers);
+        }
+
+        // GET: AspNetUsers/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var aspNetUsers = await _context.AspNetUsers
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (aspNetUsers == null)
+            {
+                return NotFound();
+            }
+
+            return View(aspNetUsers);
+        }
+
+        // POST: AspNetUsers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var aspNetUsers = await _context.AspNetUsers.FindAsync(id);
+            _context.AspNetUsers.Remove(aspNetUsers);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AspNetUsersExists(string id)
+        {
+            return _context.AspNetUsers.Any(e => e.Id == id);
+        }
+    }
+}
